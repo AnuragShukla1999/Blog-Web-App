48f6e8380ba945621f6cd673a07107285b88569c
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-02-07 19:40:07 +0530

    "Blog Web App"

---
+++ client/src/firebase.js
@@ -0,0 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-blog-b327f.firebaseapp.com',
+//   projectId: 'mern-blog-b327f',
+//   storageBucket: 'mern-blog-b327f.appspot.com',
+//   messagingSenderId: '699397991367',
+//   appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA677gDiyhe5GgDBBrfllUnObDI_-jaUPM",
+  authDomain: "blog-app-25-jan.firebaseapp.com",
+  projectId: "blog-app-25-jan",
+  storageBucket: "blog-app-25-jan.appspot.com",
+  messagingSenderId: "181873358603",
+  appId: "1:181873358603:web:6eec4fdacf3219d5ea540e",
+  measurementId: "G-PQVXPMYZ94"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
